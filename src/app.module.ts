a5c60e1598a6ce12afaf919857cb856dff10173e
matias <matias.tonello@gm2dev.com>
2023-04-28 01:20:21 -0300

    feat: environments and morgan

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
-
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+      isGlobal: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
