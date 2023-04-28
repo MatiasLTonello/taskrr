c73ac1537a8fb108c33370659152289f64242e44
matias <matias.tonello@gm2dev.com>
2023-04-28 01:27:03 -0300

    feat: configure cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
+import { CORS } from './constants/cors';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import morgan from 'morgan';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const configService = app.get(ConfigService);
+  const configService = app.get(ConfigService); // Obtain the port
 
-  console.log(configService.get('PORT'));
+  app.enableCors(CORS); // Enable cors (Cross-Origin Resource Sharing
+
+  app.use(morgan('dev')); // Logs de dev
+
+  app.setGlobalPrefix('api'); // Global prefix. Now everything will be localhost:8000/api instead of localhost:8000
   await app.listen(configService.get('PORT'));
 }
 bootstrap();
